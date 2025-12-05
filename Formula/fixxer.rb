dd86a19f72f05912f66f5f554014839f0a1f432e
Nick Howard <nick@oaklens.art>
2025-12-04 17:47:56 -0800

    Update to v1.0.1 - src layout

--- Formula/fixxer.rb
+++ Formula/fixxer.rb
@@ -3,8 +3,8 @@ class Fixxer < Formula
 
   desc "AI-powered photography workflow automation"
   homepage "https://github.com/BandwagonVibes/fixxer"
-  url "https://github.com/BandwagonVibes/fixxer/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "c7052bc1154984681a01f34a4cf5cc5dca85c6a5f69035f4e4b516f118c69a34"
+  url "https://github.com/BandwagonVibes/fixxer/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "ee1fb4a7e33332c88a61206a0550bc05c6c22f2eac354c8eed12cd0d1910b69c"
   license "MIT"
 
   depends_on "python@3.12"
